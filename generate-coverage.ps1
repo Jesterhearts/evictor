65297c9e139d04a697a586e570e399030c1f1289
Jesterhearts <Jesterhearts@users.noreply.github.com>
2025-08-17 22:38:54 -0700

    Update generate-coverage.ps1

--- generate-coverage.ps1
+++ generate-coverage.ps1
@@ -1,12 +1,13 @@
 param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$InstrProfile
 )
 
 $llvmCovPath = "$env:USERPROFILE\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\x86_64-pc-windows-msvc\bin\llvm-cov.exe"
 $targetExe = ".\target\x86_64-pc-windows-msvc\coverage\x86_64-pc-windows-msvc\release\fuzz_sieve.exe"
 $outputFile = "index.html"
+$covData = ".\fuzz\coverage\$InstrProfile\coverage.profdata"
 
-& $llvmCovPath show -format=html -instr-profile $InstrProfile -Xdemangler=rustfilt $targetExe -ignore-filename-regex=".*cargo\\registry" > $outputFile
+& $llvmCovPath show -format=html -instr-profile $covData -Xdemangler=rustfilt $targetExe -ignore-filename-regex=".*cargo\\registry" > $outputFile
 
 Write-Host "Coverage report generated: $outputFile"
